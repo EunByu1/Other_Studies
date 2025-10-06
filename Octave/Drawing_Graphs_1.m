5858f65225baf3f9902e671e41fe60c016e57f99
Eunbyul <eunbyul0805@gmail.com>
2025-10-06 18:52:05 +0900

    [practice] hold on

---
+++ Octave/Drawing_Graphs_1.m
@@ -0,0 +1,16 @@
+clc; clear; close all;
+
+x = 0:0.1:2*pi;
+y1 = sin(x);
+y2 = cos(x);
+
+figure;
+plot(x,y1, '-r', 'LineWidth', 2);
+hold on;
+plot(x,y2, '--b', 'LineWidth', 2);
+title('Sine and Cosine Wave');
+xlabel('x');
+ylabel('sin(x)');
+legend('sin(x)', 'cos(x)');
+grid on;
+hold off;
