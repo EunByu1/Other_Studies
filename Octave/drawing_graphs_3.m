2bbb710f28242ac2fa0cf8b3c821caa5c3122aef
Eunbyul <eunbyul0805@gmail.com>
2025-10-07 16:15:03 +0900

    [practice] xlim & ylim

---
+++ Octave/drawing_graphs_3.m
@@ -0,0 +1,17 @@
+clc; clear; close all;
+
+figure;
+hold on;
+grid on;
+axis equal;
+xlim([-2, 2])
+ylim([-2, 2])
+
+% linspace = linear space (시작, 끝, 몇개로 데이터 나눌지)
+x = linspace(0, 2*pi, 100);
+y = sin(x);
+
+plot(x, y, 'b-', 'LineWidth', 2);
+xlabel('X axis')
+ylabel('Y axis')
+title('Sine Wave')
